e04a4e9567add27f66d34319df65999cbce98661
poppycars22 <92829663+poppycars22@users.noreply.github.com>
2023-10-22 14:43:00 -0400

    <@553652308295155723> test

---
+++ Code/Cards/SkillPoints/BlockForceSkillPoint.cs
@@ -0,0 +1,21 @@
+﻿namespace PoppyScyyeGameModes.Cards
+{
+    internal class BlockForceSkillPoint : SkillPointCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            statModifiers.lifeSteal = 0.1f;
+            Cards.Add(this);
+        }
+
+        protected override CardInfoStat GetStat()
+        {
+            return new CardInfoStat()
+            {
+                amount = "+10%",
+                stat = "Lifesteal",
+                positive = true
+            };
+        }
+    }
+}
